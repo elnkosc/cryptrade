5b87e0331e7f327e682e9098c8b5ac34986ce594
Koen <koen@schilders.org>
2020-03-29 11:14:09 +0200

    v0.1 on github
    Signed-off-by: Koen <koen@schilders.org>

--- cryptrade.py
+++ cryptrade.py
@@ -1,22 +1,14 @@
-#!/usr/bin/python3.7
 from trade import coinbase
 from trade import binance
-from trade import logging
 from trade.parameters import CommandLine
-import sys
 import time
 import json
-
-# Define general preferences
-WAIT_TIME = 5             # refresh time (in seconds) for trade update check
-SINGLE_ORDER_WAIT = 3600  # max time before cancelling a single order (when empty orders are allowed)
 
 # store all your API credentials in cryptrade.json file!
 credentials = json.load(open("cryptrade.json", "r"))
 
 # create global object instances
 parameters = CommandLine()
-logger = logging.Logger(parameters.logging_level)
 
 # create abstract factory for API instantiation
 if parameters.exchange == "coinbase":
@@ -36,91 +28,43 @@ selling = api_factory.create_accumulator("sell")
 
 try:
     ticker.update()
-
-    # wait for right market price to start
-    wait_msg = False
-    while ticker.price > parameters.high_price or ticker.price < parameters.low_price:
-        if not wait_msg:
-            wait_msg = True
-            logger.log(logging.BASIC, f"Waiting for price to be in range {parameters.low_price:6.4f} - "
-                                      f"{parameters.high_price:6.4f}")
-        time.sleep(WAIT_TIME)
-        ticker.update()
-
     account.update(ticker.price)
 
     # start trading!
-    buy_units = parameters.basic_units
-    sell_units = parameters.basic_units
     trading = True
     while trading:
 
         ticker.update()
-        logger.log(logging.DETAILED, f"{ticker}")
 
         # make buy order
         buy_price = ticker.bid * (1 - parameters.delta)
-        buy_amount = min(max(buy_units, parameters.basic_units) * parameters.basic_amount,
-                         account.buying_amount / buy_price)
+        buy_amount = min(parameters.basic_amount, account.buying_amount / buy_price)
         buy_order = api_factory.create_order(trade_client, trade_product, "buy", buy_price, buy_amount)
-        logger.log(logging.DETAILED, f"{buy_order}")
 
         # make sales order
         sell_price = ticker.ask * (1 + parameters.delta)
-        sell_amount = min(max(sell_units, parameters.basic_units) * parameters.basic_amount,
-                          account.trading_amount)
+        sell_amount = min(parameters.basic_amount, account.trading_amount)
         sell_order = api_factory.create_order(trade_client, trade_product, "sell", sell_price, sell_amount)
-        logger.log(logging.DETAILED, f"{sell_order}")
 
-        # allow 1 failed order when empty orders are allowed
-        single_order = False
-        if buy_order.error and sell_order.error:
+        # check on failure
+        if buy_order.error or sell_order.error:
             trading = False
-        elif buy_order.error or sell_order.error:
-            trading = parameters.empty_order
-            single_order = True
 
         check_orders = True
-        total_wait = 0
         while trading and check_orders:
-            time.sleep(WAIT_TIME)
-            total_wait += WAIT_TIME
+            time.sleep(5)
 
-            if single_order and total_wait > SINGLE_ORDER_WAIT:
+            if sell_order.status():
                 check_orders = False
+                selling.add(sell_order.filled_size, sell_order.executed_value)
+                buy_order.cancel()
 
-            if not sell_order.error:
-                if sell_order.status():
-                    check_orders = False
-                    selling.add(sell_order.filled_size, sell_order.executed_value)
+            if buy_order.status():
+                check_orders = False
+                buying.add(buy_order.filled_size, buy_order.executed_value)
+                sell_order.cancel()
 
-                    if sell_order.filled_size > 0:
-                        buy_units -= 1
-                        sell_units += 1
-
-                    logger.alert(logging.BASIC, "SELL-ORDER COMPLETE", f"{sell_order}")
-
-            if not buy_order.error:
-                if buy_order.status():
-                    check_orders = False
-                    buying.add(buy_order.filled_size, buy_order.executed_value)
-
-                    if buy_order.filled_size > 0:
-                        sell_units -= 1
-                        buy_units += 1
-
-                    logger.alert(logging.BASIC, "BUY-ORDER COMPLETE", f"{buy_order}")
-
-        # cancel any (matching) order(s)
-        sell_order.cancel()
-        buy_order.cancel()
-
-        # account.update(ticker.price)
-        logger.log(logging.DETAILED, f"{account}\n{buying}\n{selling}\n")
-
-    logger.alert(logging.BASIC, "TRADING ABORTED!",
-                 f"Trading result : {selling.value - selling.fee - buying.value - buying.fee:6.2f}")
+        account.update(ticker.price)
 
 except Exception:
     trade_client.cancel_all(trade_product)
-    logger.log(logging.BASIC, f"Exception raised: {sys.exc_info()[0]}")
--- trade/parameters.py
+++ trade/parameters.py
@@ -2,20 +2,11 @@ import argparse
 
 class TradeParameters:
     def __init__(self):
-        self._logging_level = 1
         self._currency = "BTC"
         self._exchange = "coinbase"
-        self._empty_order = False
-        self._low_price = 0
-        self._high_price = 1000000
         self._delta = 1.5
         self._basic_amount = 0.001
-        self._basic_units = 1
         self._buying_currency = "EUR"
-
-    @property
-    def logging_level(self):
-        return self._logging_level
 
     @property
     def exchange(self):
@@ -30,28 +21,12 @@ class TradeParameters:
         return self._buying_currency
 
     @property
-    def empty_order(self):
-        return self._empty_order
-
-    @property
-    def low_price(self):
-        return self._low_price
-
-    @property
-    def high_price(self):
-        return self._high_price
-
-    @property
     def delta(self):
         return self._delta
 
     @property
     def basic_amount(self):
         return self._basic_amount
-
-    @property
-    def basic_units(self):
-        return self._basic_units
 
 
 class CommandLine(TradeParameters):
@@ -62,8 +37,7 @@ class CommandLine(TradeParameters):
             description="Trade automatically on Coinbase Pro in multiple crypto currencies.",
             epilog="This application will trade the selected currency and volumes. It will create a buying and\n"
                    "sales order that are a specified percentage below and over the market price. When an order\n"
-                   "is matched, a new pair of orders is created. The amount of units to trade is increased by one\n"
-                   "for the next consecutive order-type while for the other order-type it is decreased by one.\n"
+                   "is matched, a new pair of orders is created.\n"
                    "Happy trading!")
 
         # positional parameters
@@ -83,40 +57,11 @@ class CommandLine(TradeParameters):
                                  "before making a trade (accepts fractional numbers).")
         parser.add_argument("-a", "--amount", dest="trade_amount", type=float, default=0.001, action="store",
                             help="Initial amount to start trading with (btc>=0.001, eth>=0.01, xrp>=1, ltc=0.1).")
-        parser.add_argument("-u", "--units", dest="trade_units", type=int, default=1, action="store",
-                            help="Initial amount of units to start trading with (> 0).")
-        parser.add_argument("-l", "--logging", dest="logging_level", type=int, default=1,
-                            action="store", choices=[1, 2, 3],
-                            help="Logging level 0=Off, 1=Basic, 2=Detailed.")
-        parser.add_argument("-e", "--empty", dest="empty_order", action="store_true",
-                            help="When specified, allow trading when buying or sales order cannot be made due to "
-                                 "insufficient funds.")
-        parser.add_argument("-ph", "--high_price", dest="high_price", type=float, default=100000.0, action="store",
-                            help="Start trading when market price is below this value")
-        parser.add_argument("-pl", "--low_price", dest="low_price", type=float, default=0.0, action="store",
-                            help="Start trading when market price is above this value")
 
         args = parser.parse_args()
 
-        self._logging_level = args.logging_level
         self._currency = args.currency.upper()
         self._exchange = args.exchange.lower()
-        self._empty_order = args.empty_order
-        self._low_price = args.low_price
-        self._high_price = args.high_price
         self._delta = args.trade_delta / 100
         self._basic_amount = args.trade_amount
-        self._basic_units = args.trade_units
         self._buying_currency = args.buying_currency.upper()
-
-        if self._low_price < 0:
-            raise AttributeError("Invalid argument: low_price, minimum price cannot be negative")
-
-        if self._high_price < self._low_price:
-            raise AttributeError("Invalid argument: high_price, should be higher than low_price")
-
-        if self._delta <= 0 or self._delta >= 1:
-            raise AttributeError("Invalid argument: trade, trade-delta should be between 0 & 100%")
-
-        if self._basic_units <= 0:
-            raise AttributeError("Invalid argument: units, should be higher than 0")
