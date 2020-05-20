9e0ca4f37b3c23a9f4cdfb17c949266141d8df2b
Koen <koen@schilders.org>
2020-05-20 22:39:34 +0200

    Added license
    Signed-off-by: Koen <koen@schilders.org>

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='cryptrade',
     version='1.1',
     packages=['cryptrade'],
-    url='',
-    license='',
-    author='Koen',
+    url='https://github.com/elnkosc/cryptrade/',
+    license='GNU General Public License v3.0',
+    author='Koen Schilders',
     author_email='koen@schilders.org',
-    description='Basic crypto trading API with transparent access to Coinbase Pro, Binance, Kraken, and Bitfinex'
+    description='Basic crypto trading API for developing trading bots with transparent access to Coinbase Pro, Binance, Kraken, and Bitfinex'
 )
